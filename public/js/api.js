619133aae0176955ed99c7d4d9c6cff12145107f
Raissa <barata.raissa@gmail.com>
2022-12-22 15:21:44 -0300

    Primeiro commit

---
+++ public/js/api.js
@@ -0,0 +1,7 @@
+let api_key = '6ab94240e55be53bcfbbec4ec485d164';
+
+let img_url = 'https://image.tmdb.org/t/p/w500';
+
+let genres_list_http = 'https://api.themoviedb.org/3/genre/movie/list?'
+
+let movies_genres_http = 'https://api.themoviedb.org/3/discover/movie?'
